cc0841437e6e258b5cc35ae0c7d1d609bd0602cf
Kirill Kostikov <kirill.kostikov.dev@gmail.com>
2022-06-23 12:41:47 +0300

    chore(test): исправление eslint ошибки на import - переделано получение __dirname

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -1,20 +1,16 @@
 // @ts-check
 
 import { test, expect, describe } from '@jest/globals';
-import path from 'path';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
+// import path from 'path';
+// import fs from 'fs';
 
 import loadPage from '../src/page-loader';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
+// const getFixturePath = (filename) => path.join(process.cwd(), '..', '__fixtures__', filename);
+// const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
 describe('pageLoader', () => {
   test('loadPage', () => {
-    expect(loadPage()).toBe(undefined)
-  })
-})
+    expect(loadPage()).toBe(undefined);
+  });
+});
